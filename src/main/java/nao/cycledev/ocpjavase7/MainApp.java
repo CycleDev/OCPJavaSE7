b9ec5d44944284cd7c9fa4507376c4eefce5ad8b
noprysk <noprysk@n-ix.com>
2014-07-02 15:43:47 +0300

    Creates application

---
+++ src/main/java/nao/cycledev/ocpjavase7/MainApp.java
@@ -0,0 +1,8 @@
+package nao.cycledev.ocpjavase7;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Start!!!");
+    }
+}
---
+++ src/main/java/nao/cycledev/ocpjavase7/c1/javaclassdesign/JavaClassDesing.java
@@ -0,0 +1,5 @@
+package nao.cycledev.ocpjavase7.c1.javaclassdesign;
+
+public class JavaClassDesing {
+
+}
