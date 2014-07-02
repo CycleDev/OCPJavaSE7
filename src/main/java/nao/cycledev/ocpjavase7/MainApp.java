31f8ddd2d7299342a377abde13cc7d549ab1d32b
noprysk <noprysk@n-ix.com>
2014-07-02 18:23:51 +0300

    Chapter 1

--- src/main/java/nao/cycledev/ocpjavase7/MainApp.java
+++ src/main/java/nao/cycledev/ocpjavase7/MainApp.java
@@ -1,8 +1,14 @@
 package nao.cycledev.ocpjavase7;
 
+import nao.cycledev.ocpjavase7.c1.javaclassdesign.*;
+
 public class MainApp {
 
     public static void main(String[] args) {
         System.out.println("Start!!!");
+
+        System.out.println(JavaClassDesing.displayCircle1());
+        System.out.println(JavaClassDesing.displayCircle2());
+        System.out.println(JavaClassDesing.displayCircle3());
     }
 }
--- src/main/java/nao/cycledev/ocpjavase7/c1/javaclassdesign/JavaClassDesing.java
+++ src/main/java/nao/cycledev/ocpjavase7/c1/javaclassdesign/JavaClassDesing.java
@@ -2,4 +2,56 @@ package nao.cycledev.ocpjavase7.c1.javaclassdesign;
 
 public class JavaClassDesing {
 
+    public static Circle displayCircle1() {
+        return new Circle();
+    }
+
+    public static Circle displayCircle2() {
+        return new Circle(2, 2);
+    }
+
+    public static Circle displayCircle3() {
+        return new Circle(3, 3, 3);
+    }
+
 }
+
+class Shape{
+
+    protected void draw(){
+
+    }
+
+    protected void fillColor() {
+
+    }
+}
+
+class Circle extends Shape {
+
+    int xPos, yPos, radius;
+
+    Circle() {
+        xPos = 1;
+        yPos = 1;
+        radius = 1;
+    }
+
+    Circle(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    Circle(int xPos, int yPos, int radius) {
+        xPos = xPos;
+        yPos = yPos;
+        radius = radius;
+    }
+
+
+
+    public String toString() {
+        return "center: (" + xPos + ", " + yPos + "); radius: " + radius;
+    }
+}
+
