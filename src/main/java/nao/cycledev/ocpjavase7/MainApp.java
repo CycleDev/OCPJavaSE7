283dacc6658ad72dc6df8e25c5af15c3a9a84e16
Nazar Oprysk <CycleDev@gmail.com>
2015-02-15 22:25:51 +0200

    chapter 13

--- src/main/java/nao/cycledev/ocpjavase7/MainApp.java
+++ src/main/java/nao/cycledev/ocpjavase7/MainApp.java
@@ -3,15 +3,22 @@ package nao.cycledev.ocpjavase7;
 import nao.cycledev.ocpjavase7.c10.jdbc.TestCon;
 import nao.cycledev.ocpjavase7.c12.localization.LocalizationTest;
 import nao.cycledev.ocpjavase7.c13.threads.MyThread;
+import nao.cycledev.ocpjavase7.c13.threads.SyncTest;
+import nao.cycledev.ocpjavase7.c13.threads.TimeBomb;
 import nao.cycledev.ocpjavase7.c9.javaionao2.PathTest;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        Thread myThread = new Thread(new MyThread());
-        myThread.run();
-        System.out.println(myThread);
+
+        Thread myThread = new Thread(new SyncTest());
+        Thread myThread1 = new Thread(new SyncTest());
+        Thread myThread2 = new Thread(new SyncTest());
+        myThread.start();
+        myThread1.start();
+        myThread2.start();
+
 
     }
 }
---
+++ src/main/java/nao/cycledev/ocpjavase7/c13/threads/TimeBomb.java
@@ -0,0 +1,17 @@
+package nao.cycledev.ocpjavase7.c13.threads;
+
+public class TimeBomb extends Thread {
+
+    private static final int[] count = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    @Override
+    public void run() {
+        for (int i = count.length - 1; i>=0; i--){
+            try {
+                System.out.println(count[i]);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
